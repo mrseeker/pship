fafdcde56c4e6af9fd534cddded5ea95b2274370
Julius ter Pelkwijk <pelkwijk@gmail.com>
2021-12-02 10:28:52 +0100

    cleaning up bridge commands

--- commands/bridge.py
+++ commands/bridge.py
@@ -3,11 +3,9 @@ Handles all bridge-related commands
 """
 
 from evennia import default_cmds
-from world import set as setter
-from world import alerts, errors,unparse, utils,constants
+from world import alerts, errors
 from evennia import CmdSet
 from evennia.utils.search import search_object
-from evennia.utils import evtable
 
 class BridgeCmdSet(CmdSet):
         
@@ -44,18 +42,18 @@ class CmdAlerts(default_cmds.MuxCommand):
         obj = search_object(obj_x.db.ship)[0]
         if(errors.error_on_console(self.caller,obj)):
             return 0
-        if (self.args[0] == "green"):
+        if (self.args[0][0] == "g"):
             alerts.do_ship_notify(obj,"\033[5m|X|[gALERT: Green alert|n")
-        elif (self.args[0] == "yellow"):
+        elif (self.args[0][0] == "y"):
             alerts.do_ship_notify(obj,"\033[5m|X|[yALERT: Yellow alert|n")
-        elif (self.args[0] == "red"):
+        elif (self.args[0][0] == "r"):
             alerts.do_ship_notify(obj,"\033[5m|X|[rALERT: Red alert|n")
-        elif (self.args[0] == "blue"):
+        elif (self.args[0][0] == "b" and self.args[0][2] == "u"):
             alerts.do_ship_notify(obj,"\033[5m|c|[bALERT: Blue alert|n")
-        elif (self.args[0] == "black"):
+        elif (self.args[0][0] == "b" and self.args[0][2] == "a"):
             alerts.do_ship_notify(obj,"\033[5m|w|[XALERT: Black alert|n")
         else:    
-            self.caller.msg("Alert not found: " + str(self.args))
+            alerts.notify(caller,alerts.ansi_red("Alert not found: {:s}".format(' '.join(self.args))))
 
 class CmdBroadcast(default_cmds.MuxCommand):
     """
@@ -72,10 +70,9 @@ class CmdBroadcast(default_cmds.MuxCommand):
 
     def func(self):
         self.args = self.args.strip()
-        self.args = self.args.split(" ")
         caller = self.caller
         obj_x = search_object(self.caller.location)[0]
         obj = search_object(obj_x.db.ship)[0]
-        if(errors.error_on_console(self.caller,obj)):
+        if(errors.error_on_console(caller,obj)):
             return 0
-        alerts.do_ship_notify(obj,alerts.ansi_cmd(caller.name,' '.join(self.args)))
+        alerts.do_ship_notify(obj,alerts.ansi_cmd(caller.name,self.args))
