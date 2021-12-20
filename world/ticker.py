c34780d5af430f324fd283bec5fa02ee478ef956
Julius ter Pelkwijk <pelkwijk@gmail.com>
2021-12-20 19:51:59 +0100

    editing ticker

--- world/ticker.py
+++ world/ticker.py
@@ -4,4 +4,4 @@ Handles the ticker, needs to be run once...
 
 from evennia.scripts.tickerhandler import TICKER_HANDLER
 from world import iterate
-TICKER_HANDLER.add(1, iterate.do_space_db_iterate)
+TICKER_HANDLER.add(1, iterate.do_space_db_iterate, "db_iterate")
